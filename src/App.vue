cd09443177603020831faf9a6b90365d51956ed1
Martijn Dorsman <martijn.dorsman@gmail.com>
2018-04-26 22:58:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BinaryClock/>
+  </div>
+</template>
+
+<script>
+import BinaryClock from './components/BinaryClock'
+
+export default {
+  name: 'App',
+  components: {
+    BinaryClock
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BinaryClock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    {{ clock }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      clock: ''
+    }
+  },
+  methods: {
+    getTime () {
+      let bin = ''
+      let number = (Date.now() / 1000).toString()
+      this.clock = parseInt(number, 10).toString(2)
+    }
+  },
+  mounted () {
+    setInterval(this.getTime, 1000)
+  }
+}
+</script>
